6e320b9cb96ac62fded72677971997ea06e550e6
Brennan Butler <me@thefatshizms.net>
2014-12-16 23:34:15 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,18 +2,10 @@
 
 int main()
 {
-	char response =0;
-
 	if (!SetupSocket())
 	{
 		printf("failed setting up socket");
 		return 0;
-	}
-	scanf("%d", response);
-
-	if (response == 69)
-	{
-		ShutdownSocket();
 	}
 	return 0;
 }
