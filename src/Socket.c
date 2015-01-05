7d5625b9f65ef7d2e377576341fb17fe9406c88b
thefatshizms <brennan9723@hotmail.com>
2015-01-05 06:52:51 +0000

    Parsing and some cleanup

--- src/Socket.c
+++ src/Socket.c
@@ -22,48 +22,59 @@
 #include "Socket.h"
 
 char response[] = "HTTP/1.x 200 OK\n"
-"Content-Type: text/html; charset=UTF-8\nContent-Length: 100\nConnection: close\n\n"
-"<doctype !html><html><head><title>Test</title></head>"
-"<body><h1>This is a test.</h1></body></html>\n";
+"Content-Type: text/html; charset=UTF-8\nContent-Length: 100\nConnection: close\n\n";
+
+const char DEF_WWW[74] = "C:/Users/Brennan/Documents/Visual Studio 2013/Projects/FatHTTP/FatHTTP/www";
+
+#define	DEF_BUFSIZE	512
 
 int SetupSocket()
 {
 	SOCKET client;
 	WSADATA data;
-	char recvbuffer[512];
+	char recvbuffer[DEF_BUFSIZE];
 	struct sockaddr_in serverinfo;
 	int bytesReceived = 0;
 
+	// Windows only
+	// Startup the winsocks with version 2.2
+	//	
+	//	TODO: Make code cross compatible.
 	if (WSAStartup(MAKEWORD(2, 2), &data) != NO_ERROR)
 	{
 		printf("WSAStartup failed << Windows\n");
 		return 0;
 	}
 
+	// Null the struct.
 	memset(&serverinfo, 0, sizeof(serverinfo));
 
 	serverinfo.sin_family = 2;
 	serverinfo.sin_addr.s_addr = htonl(INADDR_ANY);
 	serverinfo.sin_port = htons(80);
 
-
+	// Create the socket ready for use.
 	if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
 	{
-		printf("Error sock()");
-		printf("\n%d last error \n", WSAGetLastError());
+		printf("There was an error creating the socket. [%d]\n", WSAGetLastError());
 		WSACleanup();
+
 		return 0;
 	}
 
+	// Bind the socket to port 80 ready for listening.
 	if ((bind(sock, (SOCKADDR *)&serverinfo, sizeof(serverinfo))) == SOCKET_ERROR)
 	{
-		printf("\n%d last error \n", WSAGetLastError());
+		
+		printf("There was an error binding the socket. [%d]\n", WSAGetLastError());
 		WSACleanup();
-		printf("Error binding");
+
 		closesocket(sock);
+
 		return 0;
 	}
 
+	// Start listening for connection on port 80
 	if (!SocketListen(sock, 5))
 	{
 		printf("Error on listening\n");
@@ -75,24 +86,64 @@ int SetupSocket()
 		client = accept(sock, NULL, NULL);
 
 		if (client == INVALID_SOCKET)
-			printf("\n%d last error \n", WSAGetLastError());
+			printf("There was an error creating the client socket [%d]\n", WSAGetLastError());
 
-		bytesReceived = recv(client, recvbuffer, 512, 0);
+		bytesReceived = recv(client, recvbuffer, DEF_BUFSIZE, 0);
 
 		if (bytesReceived == -1)
-			printf("\n%d last error \n", WSAGetLastError());
+			continue;
 
-		printf("%d\n\n", bytesReceived);
+		printf("Bytes recieved [%d]\n\n", bytesReceived);
 
 		recvbuffer[bytesReceived] = 0;
 
+#if defined(_DEBUG)
 		printf("%s\n", recvbuffer);
+#endif
+		//ParseRequest(&recvbuffer);
 
-		send(client, response, strlen(response), 0);
+		printf("%s", recvbuffer);
 
+		char method[10];
+		char requested[512 / 2];
+		char proto_version[20];
+		int s;
+		if (s = sscanf(&recvbuffer, "%s %s %s", &method, &requested, &proto_version) != 3)
+		{
+			printf("sscanf probrem %d\n", s);
+		}
+
+		printf("%s %s%s %s\n", method, DEF_WWW, requested, proto_version);
+
+		char requested_file[512];
+
+		strcpy(&requested_file, DEF_WWW);
+		strcat(&requested_file, requested);
+
+		FILE *file = fopen(requested_file, "r");
+		if (file == NULL)
+		{
+			send(client, "HTTP /1.x 200 OK\n"
+				"Content-Type: text/html;\ncharset=UTF-8\nContent-Length: 100\nConnection: close\n\n<html><head><title>404</title></head><body>file not found</body></html>\n", 168, 0);
+		}
+		else
+		{
+			send(client, "HTTP /1.x 200 OK\n"
+				"Content-Type: text/html;\ncharset=UTF-8\nContent-Length: 100\nConnection: close\n\n<html><head><title>404</title></head><body>file not found</body></html>\n", 168, 0);
+			fclose(file);
+		}
+		
+		//send(client, response, strlen(response), 0);
+		ConstructHeader("HTTP", "1.1", "404", "text/html", "Connection: close", "<div></div>");
 		closesocket(client);
 	}
 
+	return 1;
+}
+
+int ConstructHeader(const char *method, const char *version, const char *resp_code, const char *content_type, const char *extra_fields, const char *content)
+{
+	printf("%s %s %s %s %s", method, version, resp_code, content_type, extra_fields);
 	return 1;
 }
 
